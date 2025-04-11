7c7ed840caf31ec9507c5adc8943318826db57ff
Waqarghori <waqar.iqbal789@gmail.com>
2025-04-12 02:20:27 +0500

    upd

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
-let car = document.getElementById("car")
-let carForwerd = 10;
+
+
+
+let car = document.getElementById("car");
+let red = document.querySelector(".red");
+let green = document.querySelector(".green");
+let yellow = document.querySelector(".yellow");
+let carForward = 10;
 let timer = 0;
 let carInterval;
+let timerInterval;
+
+function play(){
+    carInterval = setInterval(function(){
+        carForward += 1;
+        car.style.left = `${carForward}px`
+    }, 10)
+    green.style.background = "#05fb05";
+
+    timerInterval = setInterval(function(){
+        timer += 1;
+        
+        if(timer > 3){
+            clearInterval(carInterval);
+            red.style.background = "red";
+            green.style.background = "#034b03";
+        }
+        if(timer === 6){
+            red.style.background = "#810b0b";
+            yellow.style.background = "yellow";
+        }
+        if(timer > 7 && timer < 11){
+            carInterval = setInterval(function(){
+                carForward += 2;
+                car.style.left = `${carForward}px`
+            }, 10)
+            green.style.background = "#05fb05";
+            yellow.style.background = "#818106";
+            red.style.background = "#810b0b";
+        }
+
+    }, 1000)
+}
+
+
 
 // window.addEventListener("keydown",function(event){
 
