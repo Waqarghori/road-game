e346eb160204390df8876a8c3e948d80b860d6eb
Waqarghori <waqar.iqbal789@gmail.com>
2025-04-11 23:33:28 +0500

    upd

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
 let car = document.getElementById("car")
 let carForwerd = 10;
+let timer = 0;
+let carInterval;
 
-window.addEventListener("keydown",function(event){
+// window.addEventListener("keydown",function(event){
 
-    console.log(event.keyCode); 
+//     console.log(event.keyCode); 
 
-    if(event.keyCode == 39 && carForwerd < 1280){
-        car.style.left = `${carForwerd}px`
-        carForwerd += 5;
-        car.style.bottom = `80px`
-         car.style.transform = "scaleX(-1)"
-         console.log(carForwerd);
+//     if(event.keyCode == 39 && carForwerd < 1280){
+//         car.style.left = `${carForwerd}px`
+//         carForwerd += 5;
+//         car.style.bottom = `80px`
+//          car.style.transform = "scaleX(-1)"
+//          console.log(carForwerd);
          
-    }else if(event.keyCode == 37 && carForwerd > 10){
-        car.style.left = `${carForwerd}px`
-        car.style.bottom = `120px`
-        car.style.transform = "scaleX(1)"
-        carForwerd += -5
-    }
-})
+//     }else if(event.keyCode == 37 && carForwerd > 10){
+//         car.style.left = `${carForwerd}px`
+//         car.style.bottom = `120px`
+//         car.style.transform = "scaleX(1)"
+//         carForwerd += -5
+//     }
+// })
+
+// function carGo(){
+//     if(carForwerd  < 1280){
+        
+//    carInterval = setInterval(function(){
+//     carForwerd += 5;
+//     car.style.left = `${carForwerd}px`
+//    }, 10)
+// }else{
+//     clearInterval(carInterval)
+//     carInterval = setInterval(function(){
+//         carForwerd -= 5;
+//         car.style.left = `${carForwerd}px`
+//        }, 10)
+//     }
+// }
