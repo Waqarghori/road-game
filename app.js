2a3010ecaf07aba614d7a6c87113c1f88acfa36a
Waqarghori <waqar.iqbal789@gmail.com>
2025-04-11 18:38:51 +0500

    upd

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-let carForwerd = 0
+let car = document.getElementById("car")
+let carForwerd = 10;
 
 window.addEventListener("keydown",function(event){
 
     console.log(event.keyCode); 
+
+    if(event.keyCode == 39 && carForwerd < 1280){
+        car.style.left = `${carForwerd}px`
+        carForwerd += 5;
+        car.style.bottom = `80px`
+         car.style.transform = "scaleX(-1)"
+         console.log(carForwerd);
+         
+    }else if(event.keyCode == 37 && carForwerd > 10){
+        car.style.left = `${carForwerd}px`
+        car.style.bottom = `120px`
+        car.style.transform = "scaleX(1)"
+        carForwerd += -5
+    }
 })
